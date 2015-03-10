df406e604648b239b43452e40a1d453d5ddcf570
frankrs <frankstapleton3d@gmail.com>
2015-03-10 08:10:34 -0400

    added new swatch UI / GamePad support

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -18,86 +18,86 @@ public class ColorChanger : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		RaycastHit hit;
-		if(Physics.Raycast(transform.position,transform.forward, out hit ,sightDist, switchable)){
-			colorSampled  = hit.collider.renderer.sharedMaterial.GetColor("_Color");
-
-			if(matName.text != hit.collider.renderer.sharedMaterial.name){
-				matName.text = hit.collider.renderer.sharedMaterial.name;
-				DestroySwatches();
-				swatchIndex = 0;
-				}
-
-			if(Input.GetKeyDown("3") && colorPannel.transform.childCount > 0){
-				hit.collider.gameObject.SendMessage("ChangeColor",availableSwatches[swatchIndex].color,SendMessageOptions.DontRequireReceiver);
-				colorName.text = availableSwatches[swatchIndex].name;
-			}
-
-			if(Input.GetKeyDown("2") && colorPannel.transform.childCount > 0){
-				swatchIndex ++;
-				if(swatchIndex == colorPannel.transform.childCount){
-					swatchIndex = 0;
-				}
-				DestroySwatches();
-				DrawSwatches(hit);
-			}
-
-			if(Input.GetKeyDown("1") && colorPannel.transform.childCount == 0){
-				DrawSwatches(hit);
-			}
-		}
-		else{
-			colorSampled = Color.white;
-			matName.text = "";
-			if(colorPannel.transform.childCount == 0){
-				return;
-			}
-			swatchIndex = 0;
-			DestroySwatches();
-		}
-	}
-
-	void OnDrawGizmos(){
-		Debug.DrawRay(transform.position,transform.forward * sightDist,Color.red);
-	}
-
-
-
-	public void DestroySwatches() {
-		foreach(GameObject s in swatchObjects){
-			Destroy(s);
-		}
-		swatchObjects.Clear();
-		colorName.text = "";
-	}
-
-
-	public void DrawSwatches(RaycastHit hit){
-		ColorSwatch cS = hit.collider.GetComponent<ColorSwatch>();
-		if (cS == null){
-			return;
-		}
-		availableSwatches = cS.swatches;
-		foreach(Swatch s in availableSwatches){
-			var swatchObject = GameObject.Instantiate(swatchPrefab,new Vector3(0f,0f,0f),Quaternion.identity) as GameObject;
-			var nC = new Color();
-			nC = s.color;
-			if(s == availableSwatches[swatchIndex]){
-				nC.a = 1f;
-				colorName.text = availableSwatches[swatchIndex].name;
-			}
-			else{
-				nC.a = .5f;
-			}
-			swatchObject.GetComponent<Image>().color = nC;
-			swatchObject.transform.SetParent(colorPannel,false);
-			swatchObjects.Add(swatchObject);
-		}
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		RaycastHit hit;
+//		if(Physics.Raycast(transform.position,transform.forward, out hit ,sightDist, switchable)){
+//			colorSampled  = hit.collider.renderer.sharedMaterial.GetColor("_Color");
+//
+//			if(matName.text != hit.collider.renderer.sharedMaterial.name){
+//				matName.text = hit.collider.renderer.sharedMaterial.name;
+//				DestroySwatches();
+//				swatchIndex = 0;
+//				}
+//
+//			if(Input.GetKeyDown("3") && colorPannel.transform.childCount > 0){
+//				hit.collider.gameObject.SendMessage("ChangeColor",availableSwatches[swatchIndex].color,SendMessageOptions.DontRequireReceiver);
+//				colorName.text = availableSwatches[swatchIndex].name;
+//			}
+//
+//			if(Input.GetKeyDown("2") && colorPannel.transform.childCount > 0){
+//				swatchIndex ++;
+//				if(swatchIndex == colorPannel.transform.childCount){
+//					swatchIndex = 0;
+//				}
+//				DestroySwatches();
+//				DrawSwatches(hit);
+//			}
+//
+//			if(Input.GetKeyDown("1") && colorPannel.transform.childCount == 0){
+//				DrawSwatches(hit);
+//			}
+//		}
+//		else{
+//			colorSampled = Color.white;
+//			matName.text = "";
+//			if(colorPannel.transform.childCount == 0){
+//				return;
+//			}
+//			swatchIndex = 0;
+//			DestroySwatches();
+//		}
+//	}
+//
+//	void OnDrawGizmos(){
+//		Debug.DrawRay(transform.position,transform.forward * sightDist,Color.red);
+//	}
+//
+//
+//
+//	public void DestroySwatches() {
+//		foreach(GameObject s in swatchObjects){
+//			Destroy(s);
+//		}
+//		swatchObjects.Clear();
+//		colorName.text = "";
+//	}
+//
+//
+//	public void DrawSwatches(RaycastHit hit){
+//		ColorSwatch cS = hit.collider.GetComponent<ColorSwatch>();
+//		if (cS == null){
+//			return;
+//		}
+//		availableSwatches = cS.swatches;
+//		foreach(Swatch s in availableSwatches){
+//			var swatchObject = GameObject.Instantiate(swatchPrefab,new Vector3(0f,0f,0f),Quaternion.identity) as GameObject;
+//			var nC = new Color();
+//			nC = s.color;
+//			if(s == availableSwatches[swatchIndex]){
+//				nC.a = 1f;
+//				colorName.text = availableSwatches[swatchIndex].name;
+//			}
+//			else{
+//				nC.a = .5f;
+//			}
+//			swatchObject.GetComponent<Image>().color = nC;
+//			swatchObject.transform.SetParent(colorPannel,false);
+//			swatchObjects.Add(swatchObject);
+//		}
 	}
 
 
--- Assets/Scripts/ColorSwatch.cs
+++ Assets/Scripts/ColorSwatch.cs
@@ -1,15 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ColorSwatch : MonoBehaviour {
 
 	public Swatch[] swatches;
+	private int swatchIndex = 0;
+	public GUIBs gUIBs;
+	public Material[] materials;
+	private int matIndex = 0;
+
+	void Start(){
+		DealSwatches();
+		NameMaterial();
+		SetSwatch();
+	}
+
+	void Update(){
+		if(Input.GetButtonDown("SwatchUp")){
+			Paint();
+		}
+		if(Input.GetButtonDown("MatUp")){
+			SwitchMat();
+		}
+	}
 
 
-	public void ChangeColor(Color c){
-		//Debug.Log(c);
-		//renderer.material.color = c;
-		renderer.sharedMaterial.SetColor("_Color",c);
+
+
+	void DealSwatches(){
+		foreach (Swatch s in swatches){
+			var swGo = GameObject.Instantiate(gUIBs.swatchPrefab,new Vector3(0f,0f,0f),Quaternion.identity) as GameObject;
+			swGo.transform.SetParent(gUIBs.swatchPannel,false);
+			swGo.GetComponent<Image>().color = s.color;
+		}
+		foreach (Swatch s in swatches){
+			var swGo = GameObject.Instantiate(gUIBs.swatchPrefab,new Vector3(0f,0f,0f),Quaternion.identity) as GameObject;
+			swGo.transform.SetParent(gUIBs.swatchTopPannel,false);
+			swGo.GetComponent<Image>().color = new Color(0f,0f,0f,0f);
+			gUIBs.topSwatches.Add(swGo);
+		}
+	}
+
+	void NameMaterial(){
+		gUIBs.matName.text = materials[matIndex].name;
+	}
+
+
+	void SetSwatch(){
+		for(int i = 0; i < swatches.Length; i++){
+			if(swatches[i].color == materials[matIndex].color){
+				gUIBs.swatchName.text = swatches[i].name;
+				swatchIndex = i;
+				gUIBs.topSwatches[i].GetComponent<Image>().color = swatches[i].color;
+			}
+			else{
+				gUIBs.topSwatches[i].GetComponent<Image>().color = new Color(0f,0f,0f,0f);
+			}
+		}
+	}
+
+	void Paint(){
+		swatchIndex ++;
+		if(swatchIndex == swatches.Length){
+			swatchIndex = 0;
+		}
+		materials[matIndex].color = swatches[swatchIndex].color;
+		SetSwatch();
+	}
+
+
+	void SwitchMat(){
+		matIndex ++;
+		if(matIndex == materials.Length){
+			matIndex = 0;
+		}
+		NameMaterial ();
+		SetSwatch();
 	}
 }
 
@@ -18,4 +86,15 @@ public class ColorSwatch : MonoBehaviour {
 public class Swatch{
 	public string name;
 	public Color color;
+}
+
+[System.Serializable]
+public class GUIBs{
+	public Text matName;
+	public Text swatchName;
+	public RectTransform swatchPannel;
+	public RectTransform swatchTopPannel;
+	public List <GameObject> topSwatches;
+	public GameObject swatchPrefab;
+
 }
